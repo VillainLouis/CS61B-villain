cd4ad38145f62f092710d9b3ebfda23190bc5b15
villainlouis <1972433854@qq.com>
2023-05-09 16:41:44 +0800

    Modify after submision Project 0: changing G -> private; adding file printer

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -38,8 +38,16 @@ public class NBody {
             StdDraw.pause(10);
             t += dt;
         }
-    }
 
+        // Textual output from filename
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        }
+    }
 
     /** Return a double corresponding to the radius of the universe in the given file*/
     public static double readRadius(String fileName) {
